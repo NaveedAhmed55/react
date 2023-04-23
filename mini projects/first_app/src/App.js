7daa230bc05ba7bea1f1e3727169617bf36249a0
Naveed Ahmed <76599103+NaveedAhmed55@users.noreply.github.com>
2023-04-24 00:57:40 +0500

    update tasks

---
+++ mini projects/first_app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Hello(){
+  return(
+    <h1>This is an h1 heading.</h1>
+  )
+}
+function App() {
+  return (
+    <div className="App">
+    <Hello />
+    </div>
+  );
+}
+
+export default App;

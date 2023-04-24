12bd58b28aef9d47d5c0b9241695d9846df6284d
Naveed Ahmed <76599103+NaveedAhmed55@users.noreply.github.com>
2023-04-25 03:39:25 +0500

    updated tasks

---
+++ mini projects/managing state/src/FruitsCounter.js
@@ -0,0 +1,7 @@
+function FruitsCounter(props) {
+    return (
+        <div>Total fruits: {props.fruits.length}</div>
+    )
+}
+
+export default FruitsCounter;
---
+++ mini projects/passing props/src/App.js
@@ -0,0 +1,11 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName='bob'/>
+        </div>
+    );
+};
+
+export default App;
